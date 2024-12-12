62d75ecb911971b0ebb725be3fca21bb233940e0
jaymar921 <jayharronabejar@gmail.com>
2024-12-12 16:42:06 +0800

    GPS Tracker | V.1

--- tracker_gps_petlocation/tracker_gps_petlocation/tracker_gps_petlocation.ino
+++ tracker_gps_petlocation/tracker_gps_petlocation/tracker_gps_petlocation.ino
@@ -1,42 +1,132 @@
-#include <SoftwareSerial.h>
+#include <NeoSWSerial.h>
+#include <TimerOne.h>
+
+#include <TinyGPS.h>
 
 // Setup software serial for sim800l
-SoftwareSerial sim800l(3,2); // TX pin (3) and RX pin (2)
+NeoSWSerial sim800l(3,6); // TX pin (3) and RX pin (6) 
 bool serialUpdating = false;
+int run_loop = 0;
+
+TinyGPS gps;
+NeoSWSerial neoGPS(7, 2); // TX pin (7) and RX pin (2)
+String gps_location_data = "Not retrieved yet...";
+
+String arduino_id = "pet_tag_1";
+String message_transport = "[empty]";
 
 void setup() {
   // put your setup code here, to run once:
-
+  delay(3000);
+  Serial.println("Booting Serial Connection..."); 
   Serial.begin(9600); // Begin the serial communication with arduino and IDE (Serial Monitor)
-
-  sim800l.begin(9600); // Begin the serial communication with arduino and sim800L
-
-  Serial.println("Booting Sim800L..."); 
-  delay(15000);
+  Serial.println("Booting Sim800L...");
+  sim800l.begin(9600); // Begin the serial communication with arduino and sim800L 
+  Serial.println("Booting NeoGPS..."); 
+  Serial.println(TinyGPS::library_version());
+  neoGPS.begin(9600); // Begin the serial communication  with arduino and neoGPS
   
-  Serial.println("Initializing SMS Reciever"); 
-  delay(5000);
-  initializeSmsRecieved();
+  neoGPS.listen();
+  loadGPSData();
+
+  Serial.println("Initializing SMS Receiver"); 
+  delay(2000);
+
+
+  sim800l.listen();
+  initializeSmsReceiver();
+
+  delay(1000);
+  Serial.println("Device is now running\n"); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(!serialUpdating)
-    updateSerial();
+  sim800l.listen();
+  updateSerial();
+
+  if(run_loop++ >= 100){
+    delay(20);
+    neoGPS.listen();
+    loadGPSData();
+    run_loop = 0;
+    delay(20);
+    sim800l.listen();
+  }
 }
 
-void initializeSmsRecieved(){
-  serialUpdating = true;
+void loadGPSData(){
+  //Serial.println("\nTransmitting/Receiving GPS Data");
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (neoGPS.available())
+    {
+      char c = neoGPS.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print(" SAT=");
+    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.print(" PREC=");
+    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+
+    gps_location_data = "lat: ";
+    gps_location_data.concat(flat);
+    gps_location_data.concat(" , lon: ");
+    gps_location_data.concat(flon);
+    
+    message_transport = "pinName=" + arduino_id + "&lat=";
+    message_transport.concat(flat);
+    message_transport.concat("&long=");
+    message_transport.concat(flon);
+
+    Serial.println("\nLocation Updated");
+  }else{
+    gps_location_data = "{location data was not fetched, GPS Module might have no signal}";
+    message_transport = "pinName=" + arduino_id + "&lat=0.000&long=0.000";
+    
+    Serial.println("\nSatellite Not Available");
+  }
+  
+  gps.stats(&chars, &sentences, &failed);
+  // Serial.print(" CHARS=");
+  // Serial.print(chars);
+  // Serial.print(" SENTENCES=");
+  // Serial.print(sentences);
+  // Serial.print(" CSUM ERR=");
+  // Serial.println(failed);
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+
+  
+  //Serial.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+}
+
+void initializeSmsReceiver(){
   sim800l.println("AT"); //Once the handshake test is successful, it will back to OK
   updateSerial();
   sim800l.println("AT+CMGF=1"); // Configuring TEXT mode
   updateSerial();
   sim800l.println("AT+CNMI=2,2,0,0,0"); // Decides how newly arrived SMS messages should be handled
-  serialUpdating = false;
+  updateSerial();
 }
 
 void sendSms(String to, String msg){
-  serialUpdating = true;
   sim800l.println("AT"); //Once the handshake test is successful, it will back to OK
   updateSerial();
   sim800l.println("AT+CMGF=1"); // Configuring TEXT mode
@@ -47,7 +137,6 @@ void sendSms(String to, String msg){
   updateSerial();
   sim800l.write(26);
   updateSerial();
-  serialUpdating = false;
 }
 
 void updateSerial(){
@@ -83,7 +172,13 @@ void updateSerial(){
     Serial.println("Message: " + str4);
 
     if(contents.indexOf("UPDATE") > 0)
-      sendSms(mobileNo, "Updated location: location x y z");
+      sendSms(mobileNo, "The updated GPS Location of arduino id ("+arduino_id+"): " + gps_location_data);
+    if(contents.indexOf("LOCATE") > 0){
+      sendSms(mobileNo, "To locate your pet copy the code below and paste in our website: \n\n" + message_transport);
+    }
+    if(contents.indexOf("TAG") > 0){
+      sendSms(mobileNo, message_transport);
+    }
     else sendSms(mobileNo, "Invalid Command :/");
   }
 }
