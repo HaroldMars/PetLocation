895ff0cd51c4fddc02340d1412109d512193f20b
HaroldMars <abejar199@gmail.com>
2025-02-04 22:33:58 +0800

    Update all code

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,9 +5,20 @@ export default function About() {
   return (
     <div className='bg-primary-1 w-screen h-screen text-white'>
     <Header />
-    <div className="relative md:grid md:grid-cols-6 w-[50%] h-[50%] bg-primary-2 left-[50%] translate-x-[-50%] rounded-xl">
-        <div className=" md:grid md grid-cols-8 w-[370px] h-[100%] bg-primary-2 left-[100px] translate-x-[50px] rounded-sm">
-            <h1 className= "mt-10 text-center font-serif font-bold text-3xl w-[370px]"> About </h1> 
+    <div className="relative md:grid md:grid-cols-6 w-[50%] h-[80%] bg-primary-2 left-[50%] translate-x-[-50%] rounded-xl">
+        <div className="w-[370px] h-[100%] bg-primary-2 left-[500px] translate-x-[50px] rounded-sm">
+            <h1 className= "  mt-10 text-center font-serif font-bold text-3xl w-[550px]"> About </h1> 
+            <table className=" text-center font-san-serif w-[150%] h-[50%] ">
+              
+              <tr>This research is focused on developing a system to monitor the safety of dogs, 
+                particularly in cases where a dog may go missing. The primary objective is to ensure that pet owners can track their dogs and respond promptly if they are lost.
+                However, our prototype currently has limitations due to the accuracy of the signals received by the Arduino system. While the tracking may not yet be fully reliable,
+                we assure all users and pet owners that the safety and well-being of their pets remain our top priority.
+                As this is our first prototype, there are certain constraints, and we acknowledge that improvements are necessary. Nevertheless, we are committed to advancing our research and refining the technology. 
+                In the future, we plan to update the system and address current limitations, ensuring a more accurate and effective solution for pet monitoring. We are confident that with continued development, 
+                we will achieve a more reliable tracking system for pet safety.</tr>
+              
+            </table>
         </div>
     </div>
    
--- src/Pages/Help.jsx
+++ src/Pages/Help.jsx
@@ -5,9 +5,19 @@ export default function Help() {
   return (
     <div className='bg-primary-1 w-screen h-screen text-white'>
     <Header />
-    <div className="relative md:grid md:grid-cols-6 w-[50%] h-[50%] bg-primary-2 left-[50%] translate-x-[-50%] rounded-xl">
-        <div className=" md:grid md grid-cols-8 w-[370px] h-[100%] bg-primary-2 left-[100px] translate-x-[50px] rounded-sm">
-            <h1 className= "mt-10 text-center font-serif font-bold text-3xl w-[370px]"> HELP </h1> 
+    <div className="relative md:grid md:grid-cols-6 w-[50%] h-[80%] bg-primary-2 left-[50%] translate-x-[-50%] rounded-xl">
+        <div className="  w-[370px] h-[100%] bg-primary-2 left-[100px] translate-x-[50px] rounded-sm">
+        <h3 className=" mt-10 font-serif font-bold w-[100%]  ">What Can I Help You With?</h3>
+    <p>If you need assistance or encounter any problems while using the website, please don’t hesitate to contact us at:</p>
+    <ul>
+        <li><strong>Globe SIM</strong>: +639273865959</li>
+        <li><strong>Other Number</strong>: +63991436531</li>
+    </ul>
+    <table className="  mt-2  w-[100%] h-[10%]">
+      <tr>
+      If you experience a bug or an issue, please report it to us immediately so that we can fix it as soon as possible. This helps us improve the website and ensures a safe experience for both the users and the owners.
+      </tr>
+    </table>
         </div>
     </div>
    
